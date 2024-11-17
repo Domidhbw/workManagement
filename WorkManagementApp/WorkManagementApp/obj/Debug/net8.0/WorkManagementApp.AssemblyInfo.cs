3fe16f5de250af8bc426dba28e12a88003d36887
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-11-17 18:21:23 +0100

    pull

--- WorkManagementApp/WorkManagementApp/obj/Debug/net8.0/WorkManagementApp.AssemblyInfo.cs
+++ WorkManagementApp/WorkManagementApp/obj/Debug/net8.0/WorkManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e9df7a8749b08c043d9abbf5deb4d67ee39cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bac3aa74d427485c5153cc7216f024c0bb5e1ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
